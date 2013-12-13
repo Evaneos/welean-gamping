7fb68d6918e4d5c604d039e7731cb77cae357e0d
Olivier <olivier@evaneos.com>
2013-12-13 12:03:39 +0100

    sql files and test fetcher

--- app/Gamping/Controllers/Home.php
+++ app/Gamping/Controllers/Home.php
@@ -9,12 +9,17 @@ class Home extends \Gamping\Controller
     {
         $this->getAllParams();
 
+
+        $fetcher = new \Berthe\Fetcher(-1, -1);
+        $resultSet = $this->userManager->getByPaginator($fetcher)->getResultSet();
+        var_dump($resultSet);
+
         // var_dump($this->userManager);
         var_dump($this->userManager->getById(2));
 
         return array('data' => 'Hello World');
     }
-    
+
     public function afterRun(array $data)
     {
         return $data;
