96090265d9f3fb1552ab02ca9dad28e8ec4bab15
Olivier <olivier@evaneos.com>
2013-12-15 00:58:48 +0100

    fix allowed, fix save hosting

--- app/Gamping/Model/ParcelHasCommodity/Manager.php
+++ app/Gamping/Model/ParcelHasCommodity/Manager.php
@@ -5,4 +5,10 @@ class Manager extends \Berthe\AbstractManager {
     public function getVoForCreation() {
         return new VO();
     }
+
+    public function getByParcelId($id) {
+        $fetcher = new \Berthe\Fetcher(-1, -1);
+        $fetcher->addFilter('parcel_id', \Berthe\Fetcher::TYPE_EQ, $id);
+        return $this->getByPaginator($fetcher)->getResultSet();
+    }
 }
