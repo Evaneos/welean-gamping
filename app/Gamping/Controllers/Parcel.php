279bc54e0f74db39283c8baa4bd702bb14522614
Olivier <olivier@evaneos.com>
2013-12-15 00:33:34 +0100

    parcel page

--- app/Gamping/Controllers/Parcel.php
+++ app/Gamping/Controllers/Parcel.php
@@ -5,8 +5,30 @@ use Gamping\Controller;
 
 class Parcel extends Controller
 {
+    public $userManager;
+    public $parcelManager;
+    public $activityManager;
+    public $commodityManager;
+
+    public $countryManager;
+    public $regionManager;
+
+    public $parcelHasActivityManager;
+    public $parcelHasCommodityManager;
+
+
     protected function executeAction()
     {
+        $id = $this->getParam('id', null);
+        if ($id) {
+            $parcel = $this->parcelManager->getById($id);
+        }
+
+        $this->setData('commodities', $this->commodityManager->getAll());
+        $this->setData('activities', $this->activityManager->getAll());
+        $this->setData('phc', $this->parcelHasCommodityManager->getByParcelId($id));
+        $this->setData('pha', $this->parcelHasActivityManager->getByParcelId($id));
+        $this->setData('parcel', $parcel);
         $this->setData('bodyClass', 'parcel');
     }
 
